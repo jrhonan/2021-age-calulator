c7a69faa1cbae3d61f81e0060ee3d61adbdf6ffd
jrhonan <70732788+jrhonan@users.noreply.github.com>
2021-02-16 21:00:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function age_calculator(member, name, age){
 age_calculator("mom", "Patricia" "59")
 
 console.log(age_calculator("mom", "Patricia" "59")
-console.log(age_calculator("sister", "Caitlin" "59")
+console.log(age_calculator("sister", "Caitlin" "32")
